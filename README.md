caf6172cca01864095016800f2e43c266429405c
root <root@cloudy.(none)>
2014-09-26 00:07:31 -0400

    Alpha

---
+++ README.md
@@ -0,0 +1,9 @@
+builder - a simple game of time and resources
+written in javascript (jQuery)
+by Ryan Hartje http://github.com/ryanhartje/
+
+
+Resources
+---
+Font Awesome [http://fortawesome.github.io/Font-Awesome/icons/]
+
--- static/js/builder.js
+++ static/js/builder.js
@@ -1,37 +1,59 @@
+/* Builder.js 
+ * simple game of time and resources
+ *
+ * Ryan Hartje - github.com/ryanhartje
+ */
+// Test using score to resize things
+//$('.score').text(Math.round(score));
+
 $(document).ready(function(){
-    $('.score').text(jQuery.now());
+  // Hide Cathub before we start
+  $('.cathub').toggle();
+
     // Instansiate variables
     // spawn is our initial clock time
     // multi - multipliar for causing noticiable increase
     // init - inital font size
     var spawn = jQuery.now(),
-        score = 14,
-        multi = 1;   
+        score = 1,
+        multi = 24;   
     
-
-    
+    // The following defines our interval loop 
     var refreshScore = setInterval(function(){
       //functions
       // Bonuses and more will add to the score
-      var bonus = function(multi,points){
-        multi += points;
-        return multi;
+      var bonus = function(score,points){
+        score += points;
+        return score;
       };
         
-      //call backs
-      $('.score').click(function(){
-        $('.warn').text(jQuery.now());
-        score += bonus(multi,1);  
+      //CALL BACK$!
+      $('.click').click(function(){
+        score += 2*multi;  
       });
         
       //score logic
-      score *= (jQuery.now()/spawn)*1.0001;
+      multi *= Math.round((jQuery.now()/spawn)*1.0001);
         
-      $('.multi').text("Multi: " + multi);
-      $('.score').text(Math.round(score));
-      $('.score').css('font-size',score+'px');
+      // Update the DOM
+      $('.score').text(score);
+      $('.multi').text(multi);
       $('.spawn').text(spawn);
-      multi += 1;
+
+      // Style the money
+      $('.size').css('font-size',multi+"px");
+
+      //Score logic
+      score += 1;
+
+      if(score % 10 == 0){
+        multi += 1
+      }
+
+      if(score % 100 == 0){
+        $('.click').toggle();
+        $('.cathub').toggle();
+      }
     }, 100);
  
     refreshScore(); //mainloop
